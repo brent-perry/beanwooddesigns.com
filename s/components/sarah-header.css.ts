775c609ba7e80223e1be1fd71f2c5ccbdf9255bd
brent-perry <53350346+brent-perry@users.noreply.github.com>
2021-08-09 09:12:39 -0700

    updated

--- s/components/sarah-header.css.ts
+++ s/components/sarah-header.css.ts
@@ -1,7 +1,12 @@
 import {css} from "lit"
 export default css`
-	h1 {
-		color: green;
+	h1, p {
+		color: #000000;
 	}
 
+	div {
+		background-color: #c5b807;
+		padding: 1rem;
+		margin-right: 1rem;
+	}
 `
