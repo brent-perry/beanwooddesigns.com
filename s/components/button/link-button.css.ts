e246414f676d83b382bf05bc4dc8848dcea73fbf
brent-perry <53350346+brent-perry@users.noreply.github.com>
2022-04-09 21:03:10 -0700

    updated nav and header

--- s/components/button/link-button.css.ts
+++ s/components/button/link-button.css.ts
@@ -12,12 +12,21 @@ export default css`
 		padding: 0;
 	}
 
-	img {
+	/* img {
 		box-shadow: 10px 10px 5px #cccccc;
 		border: solid #ffffff 2px;
 		height: 190px;
 		width: 300px;
 		background-repeat: no-repeat;
+	} */
+
+	img {
+		transition: all .4s ease-out;
+		width: 300px;
+		height: 300px;
+		cursor: pointer;
+		border: 2px solid #000000;
+		object-fit: cover;
 	}
 
 	a {
